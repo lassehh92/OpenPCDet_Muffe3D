991c045574a46682c2d9d0d76237e6331e4aaf48
Lasse Hansen <lhh@civil.aau.dk>
2024-04-26 00:04:22 +0530

    experiments setup epocchs and batch size

--- tools/experiments.sh
+++ tools/experiments.sh
@@ -12,7 +12,7 @@ do
     for EPOCH in "${EPOCH_VALUES[@]}"
     do
         echo "Starting training for ${EPOCH} epochs with batch size ${BATCH_SIZE}..."
-        python train.py --cfg_file cfgs/custom_models/pointrcnn_original.yaml --epochs $EPOCH --batch_size $BATCH_SIZE --extra_tag "exp_${EPOCH}_epochs_${BATCH_SIZE}_batch" > "logs/exp_${EPOCH}_${BATCH_SIZE}.log" 2>&1
+        python train.py --cfg_file cfgs/custom_models/pointrcnn_base.yaml --epochs $EPOCH --batch_size $BATCH_SIZE --extra_tag "exp_${EPOCH}_epochs_${BATCH_SIZE}_batch" > "logs/exp_${EPOCH}_${BATCH_SIZE}.log" 2>&1
         echo "Training for ${EPOCH} epochs with batch size ${BATCH_SIZE} completed."
     done
 done
