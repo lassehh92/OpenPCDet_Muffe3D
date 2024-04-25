e98c91850c3edd792c2f2860337d1d5532d30a04
Lasse Hansen <lhh@civil.aau.dk>
2024-04-26 01:27:34 +0530

    experiments setup epocchs and batch size

--- tools/experiment_last_checkpoint.sh
+++ tools/experiment_last_checkpoint.sh
@@ -9,7 +9,7 @@ MAX_EPOCH=50
 # Set a single batch size
 BATCH_SIZE=4
 # Define the base directory for checkpoints
-BASE_CKPT_DIR="/path/to/your/checkpoints"
+BASE_CKPT_DIR="/home/lasse/Git/OpenPCDet_Muffe3D/output/custom_models/pointrcnn_base"
 
 # Start from the initial epoch and increase by the increment until reaching or exceeding the max epoch
 for ((EPOCH=$INITIAL_EPOCH; EPOCH<=$MAX_EPOCH; EPOCH+=$EPOCH_INCREMENT))
